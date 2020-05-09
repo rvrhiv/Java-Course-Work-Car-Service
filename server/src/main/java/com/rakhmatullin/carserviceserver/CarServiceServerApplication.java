31c8449baa3ae4e97fbc6002ba7734d36247969f
Aleksander Rakhmatullin <rar130600@mail.ru>
2020-05-10 00:20:02 +0300

    initial gradle commit

---
+++ server/src/main/java/com/rakhmatullin/carserviceserver/CarServiceServerApplication.java
@@ -0,0 +1,13 @@
+package com.rakhmatullin.carserviceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarServiceServerApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/rakhmatullin/carserviceserver/CarServiceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakhmatullin.carserviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
