9b3b373993f960790d73e1f85288ad9923617288
Aleksander Rakhmatullin <rar130600@mail.ru>
2020-05-16 18:33:28 +0300

    Client🌕: submit form for MastersTable

---
+++ client/src/components/forms/FormMastersTable.js
@@ -0,0 +1,84 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Navbar from "react-bootstrap/Navbar";
+import NavItem from "react-bootstrap/NavItem";
+import {addData} from "../../actions/AddData";
+
+class FormMastersTable extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            validated: false
+        }
+    }
+
+    handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        } else if (form.checkValidity() === true) {
+            const nameLine = form.elements.first_name.value + " " + form.elements.last_name.value;
+            const master = {
+                name: nameLine
+            }
+            addData(this.props.whichTable, master)
+                .then()
+                .catch(message => console.log(message));
+        }
+        this.setState({
+            validated: true
+        })
+    }
+
+    render() {
+        return (
+            <Form noValidate validated={this.state.validated} onSubmit={this.handleSubmit}>
+                <Form.Row>
+                    <Form.Group as={Col} md="6" controlId="validationCustom01">
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control
+                            required
+                            type="text"
+                            name="first_name"
+                            pattern="^[a-zA-Z]{1,40}"
+                            placeholder="First Name"
+                            autoComplete="off"
+                        />
+                        <Form.Control.Feedback type="valid">Looks nice!</Form.Control.Feedback>
+                        <Form.Control.Feedback type="invalid">
+                            This field is required! Maximum length 40 characters.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group as={Col} md="6" controlId="validationCustom01">
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control
+                            required
+                            type="text"
+                            name="last_name"
+                            pattern="^[a-zA-Z]{1,40}"
+                            placeholder="Last Name"
+                            autoComplete="off"
+                        />
+                        <Form.Control.Feedback type="valid">Looks nice!</Form.Control.Feedback>
+                        <Form.Control.Feedback type="invalid">
+                            This field is required! Maximum length 40 characters.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Form.Row>
+                <Navbar className="justify-content-end">
+                    <NavItem className="ml-1">
+                        <Button variant="danger" onClick={this.props.onHide}>Cancel</Button>
+                    </NavItem>
+                    <NavItem className="ml-1">
+                        <Button type="submit" variant="primary">Submit form</Button>
+                    </NavItem>
+                </Navbar>
+            </Form>
+        );
+    }
+}
+
+export default FormMastersTable;
--- client/src/components/modals/NewItemModal.js
+++ client/src/components/modals/NewItemModal.js
@@ -1,9 +1,18 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
+
 import FormCarsTable from "../forms/FormCarsTable";
+import FormMastersTable from "../forms/FormMastersTable";
 
 function NewItemModal(props) {
     const {whichTable, ...rest} = props;
+
+    const formFor = {
+        cars: () => <FormCarsTable onHide={props.onHide} whichTable={whichTable}/>,
+        masters: () => <FormMastersTable onHide={props.onHide} whichTable={whichTable}/>,
+        services: () => null,
+        works: () => null
+    };
 
     return (
         <Modal
@@ -12,11 +21,11 @@ function NewItemModal(props) {
         >
             <Modal.Header closeButton>
                 <Modal.Title>
-                    Create new Item
+                    Create new {whichTable.slice(0,-1)}
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <FormCarsTable onHide={props.onHide} whichTable={whichTable}/>
+                {formFor[whichTable]()}
             </Modal.Body>
             <Modal.Footer>
             </Modal.Footer>
