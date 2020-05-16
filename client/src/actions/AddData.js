5dde8599787531c76d46925b26a96588493977ca
Aleksander Rakhmatullin <rar130600@mail.ru>
2020-05-16 23:24:34 +0300

    Client🌕: Fixed adding already exists records

--- client/src/actions/AddData.js
+++ client/src/actions/AddData.js
@@ -1,5 +1,49 @@
 
+const fields = {
+    cars: "num",
+    masters: "name",
+    services: "name",
+};
+
 export async function addData(whitchTable, data) {
+    const tableData = await fetch("/api/" + whitchTable, {
+        method: "GET",
+        dataType: "JSON",
+        headers: {
+            "Content-Type": "application/json",
+        }
+    });
+
+    const jsonTableData = await tableData.json();
+
+    if (whitchTable !== "works") {
+        let alreadyExists = false;
+        jsonTableData.map((item) => {
+            if (item[fields[whitchTable]] === data[fields[whitchTable]]) {
+                alreadyExists = true;
+            }
+        });
+
+        if (alreadyExists) {
+            alert("Record with " + whitchTable + " " + fields[whitchTable] + ": " + data[fields[whitchTable]] + " already exists. Record don't will be added.");
+            return;
+        }
+    } else if (whitchTable === "works") {
+        let alreadyExists = false;
+        jsonTableData.map((item) => {
+            if ((item.date_work === data.date_work)
+                && (item.car.id === data.car.id)
+                && (item.master.id === data.master.id)
+                && (item.service.id === data.service.id)) {
+                alreadyExists = true;
+            }
+        });
+
+        if (alreadyExists) {
+            alert("Record with this work already exists. Record don't will be added.");
+            return;
+        }
+    }
 
     const response = await fetch('/api/' + whitchTable, {
         method: "POST",
