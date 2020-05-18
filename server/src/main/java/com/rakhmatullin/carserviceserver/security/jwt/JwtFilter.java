3ed13e8b824c35b7b8bbacc9fa41a91ec38773ee
Aleksander Rakhmatullin <rar130600@mail.ru>
2020-05-18 20:52:46 +0300

    Server: added class JwtFilter

---
+++ server/src/main/java/com/rakhmatullin/carserviceserver/security/jwt/JwtFilter.java
@@ -0,0 +1,39 @@
+package com.rakhmatullin.carserviceserver.security.jwt;
+
+import com.rakhmatullin.carserviceserver.exception.InvalidJwtAuthenticationException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtFilter extends GenericFilterBean {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request); //check for token
+        try {
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication auth = jwtTokenProvider.getAuthentication(token);
+                if (auth != null) {
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
+            }
+        } catch (InvalidJwtAuthenticationException exception) {
+            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, exception.getMessage());
+        }
+        chain.doFilter(request, response);
+    }
+}
