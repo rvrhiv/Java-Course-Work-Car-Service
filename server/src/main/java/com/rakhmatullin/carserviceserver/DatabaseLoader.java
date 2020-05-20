84943a84cc65e237861216e073c99f33233c019c
Aleksander Rakhmatullin <rar130600@mail.ru>
2020-05-20 23:48:42 +0300

    Server🌑: New loaded data

--- server/src/main/java/com/rakhmatullin/carserviceserver/DatabaseLoader.java
+++ server/src/main/java/com/rakhmatullin/carserviceserver/DatabaseLoader.java
@@ -46,26 +46,34 @@ public class DatabaseLoader implements CommandLineRunner {
 
         Cars bmwI8 = new Cars("A000AA00", "#000000", "BMW", true);
         Cars ladaVesta = new Cars("A001AA00", "#FFFFFF", "LADA", false);
+        Cars nissan = new Cars("K777AK111", "#B3E731", "NISSAN", true);
         carsRepository.save(bmwI8);
         carsRepository.save(ladaVesta);
+        carsRepository.save(nissan);
 
         Masters alex = new Masters("Alex Rakhmatullin");
         Masters ilya = new Masters("Ilya Gusarov");
         Masters vlad = new Masters("Vladislav Zibkin");
+        Masters serega = new Masters("Sergey Kozhin");
         mastersRepository.save(alex);
         mastersRepository.save(ilya);
         mastersRepository.save(vlad);
+        mastersRepository.save(serega);
 
-        Services updateOil = new Services("update oil", 1000, 2000);
-        Services washing = new Services("washing", 350, 550);
-        Services engineRepair = new Services("engine repair", 8000, 10000);
-        Services wheelRepair = new Services("wheel repair", 3000, 5000);
-        servicesRepository.save(updateOil);
+        Services changeOil = new Services("Change oil", 1000, 2000);
+        Services washing = new Services("Washing", 350, 550);
+        Services engineRepair = new Services("Engine repair", 8000, 10000);
+        Services wheelRepair = new Services("Wheel repair", 3000, 5000);
+        Services repainting = new Services("Repainting", 3000, 10000);
+        Services checkUp = new Services("Check Up", 6000, 9000);
+        servicesRepository.save(changeOil);
         servicesRepository.save(washing);
         servicesRepository.save(engineRepair);
         servicesRepository.save(wheelRepair);
+        servicesRepository.save(repainting);
+        servicesRepository.save(checkUp);
 
-        Works work1 = new Works(Date.valueOf("2020-05-11"), alex, bmwI8, updateOil);
+        Works work1 = new Works(Date.valueOf("2020-05-11"), alex, bmwI8, changeOil);
         Works work2 = new Works(Date.valueOf("2020-05-11"), alex, bmwI8, washing);
         Works work3 = new Works(Date.valueOf("2020-05-12"), ilya, ladaVesta, wheelRepair);
         Works work4 = new Works(Date.valueOf("2020-05-13"), vlad, ladaVesta, engineRepair);
